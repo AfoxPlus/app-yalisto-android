67fd0ba63d4fcd87b076add4263001ddd942d2d5
Valentin Mendoza Torres <jmendozat13@gmail.com>
2023-05-15 22:36:54 -0500

    Develop to Master (#5)
    * se agrego el splash (#1)
    
    * Integration (#2)
    
    * RC01.2022 to develop (#4)
    
    * update dependencies
    
    * Enhacement project (#3)
    
    * WIP update project
    
    * WIP delete yml android-qa
    
    * WIP update yml
    
    * WIP add filter.gradle and was updated build.gradle
    
    ---------
    
    Co-authored-by: Petter Garcia <petter.infante@macys.com>
    Co-authored-by: Carlos Osorio <hacybeyker@gmail.com>
    
    ---------
    
    Co-authored-by: Carlos Osorio <hacybeyker@gmail.com>
    Co-authored-by: Petter Garcia <petter.infante@macys.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,10 +3,13 @@ plugins {
     id("kotlin-android")
     id("kotlin-kapt")
     id("kotlin-parcelize")
+    id("dagger.hilt.android.plugin")
 }
 
-apply(from = "sonarqube.gradle")
-apply(from = "jacoco.gradle")
+apply {
+    from("sonarqube.gradle")
+    from("jacoco.gradle")
+}
 
 android {
     compileSdk = Versions.compileSdkVersion
@@ -17,8 +20,29 @@ android {
         minSdk = Versions.minSdkVersion
         targetSdk = Versions.targetSdkVersion
         versionCode = ConfigureApp.versionCode
-        versionName = ConfigureApp.version
+        versionName = ConfigureApp.versionName
         testInstrumentationRunner = Versions.testInstrumentationRunner
+        renderscriptSupportModeEnabled = true
+        vectorDrawables.useSupportLibrary = true
+    }
+
+    testOptions {
+        animationsDisabled = true
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = findProperty("SIGNING_KEY_ALIAS_YALISTO") as String?
+                ?: System.getenv("SIGNING_KEY_ALIAS")
+            keyPassword = findProperty("SIGNING_KEY_PASSWORD_YALISTO") as String?
+                ?: System.getenv("SIGNING_KEY_PASSWORD")
+            storeFile = file("../.signing/release-yalisto-key.jks")
+            storePassword = findProperty("SIGNING_STORE_PASSWORD_YALISTO") as String?
+                ?: System.getenv("SIGNING_STORE_PASSWORD")
+        }
     }
 
     buildTypes {
@@ -26,22 +50,22 @@ android {
             isDebuggable = false
             isMinifyEnabled = true
             isShrinkResources = true
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            buildConfigField("String", "EXAMPLE_FIELD", "\"example-release\"")
         }
-
         getByName("debug") {
             isDebuggable = true
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            buildConfigField("String", "EXAMPLE_FIELD", "\"example-debug\"")
         }
     }
 
@@ -50,23 +74,75 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
+    kotlinOptions.jvmTarget = "${JavaVersion.VERSION_11}"
 
-    buildFeatures { viewBinding = true }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+
+    bundle {
+        language {
+            enableSplit = true
+        }
+        density {
+            enableSplit = true
+        }
+        abi {
+            enableSplit = true
+        }
+    }
 
     lint {
-        isCheckDependencies = true
+        disable.addAll(
+            listOf(
+                "TypographyFractions",
+                "TypographyQuotes",
+                "JvmStaticProvidesInObjectDetector",
+                "FieldSiteTargetOnQualifierAnnotation",
+                "ModuleCompanionObjects",
+                "ModuleCompanionObjectsNotInModuleParent"
+            )
+        )
+        checkDependencies = true
+        abortOnError = false
+        ignoreWarnings = false
     }
 }
 
 dependencies {
     implementation(fileTree("libs") { include(listOf("*.jar", "*.aar")) })
-    implementation(Deps.Jetpack.kotlin)
     implementation(Deps.Jetpack.core)
+    implementation(Deps.Jetpack.kotlin)
+    implementation(Deps.Jetpack.activity)
+    implementation(Deps.Jetpack.fragment)
     implementation(Deps.Jetpack.appcompat)
+
     implementation(Deps.UI.materialDesign)
     implementation(Deps.UI.constraintLayout)
+    implementation(Deps.UI.glide)
+    kapt(Deps.UI.glideCompiler)
+    implementation(Deps.UI.uikit)
+
+    implementation(Deps.Arch.zxingAndroid) { isTransitive = false }
+    implementation(Deps.Arch.zxingCore)
+    implementation(Deps.Arch.network)
+    implementation(Deps.Arch.retrofit2)
+    implementation(Deps.Arch.gson)
+    implementation(Deps.Arch.loggingInterceptor)
+    implementation(Deps.Arch.coroutinesCore)
+    implementation(Deps.Arch.hiltAndroid)
+    kapt(Deps.Arch.hiltCompiler)
+
+    implementation(Deps.Arch.home)
+    implementation(Deps.Arch.products)
+    implementation(Deps.Arch.restaurants)
+    implementation(Deps.Arch.orders)
+
     testImplementation(Deps.Test.jUnit)
+    testImplementation(Deps.Test.testCore)
+    testImplementation(Deps.Test.truth)
+    testImplementation(Deps.Test.mockitoKotlin)
     androidTestImplementation(Deps.Test.androidJUnit)
     androidTestImplementation(Deps.Test.espresso)
 }
---
+++ app/src/main/java/com/afoxplus/yalisto/delivery/views/activities/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.afoxplus.yalisto.delivery.views.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.afoxplus.home.delivery.flow.HomeFlow
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class SplashActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var homeFlow: HomeFlow
+
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        lifecycleScope.launchWhenCreated {
+            delay(1500)
+            homeFlow.goToHome(this@SplashActivity)
+        }
+    }
+}
