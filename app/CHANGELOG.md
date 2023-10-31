9089e46aa1c84d1e7cc5e72df8e4bca30941bf43
Carlos Osorio <hacybeyker@gmail.com>
2023-10-31 17:53:52 -0500

    update

--- app/CHANGELOG.md
+++ app/CHANGELOG.md
@@ -1,5 +1,9 @@
 # app-afoxplus-android Changelog
 
+## Version 1.2.0
+### Added
+- Deploy RC02.2023"
+
 ## Version 1.0.0
 ### Added
 - Se agrego flujo de ordenes.
--- buildSrc/src/main/kotlin/ConfigureApp.kt
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -3,8 +3,8 @@ object ConfigureApp {
     const val organization = "afoxplus"
     const val projectName = "app-yalisto-android"
     const val applicationId = "com.afoxplus.yalisto"
-    const val versionCode = 1
-    const val version = "1.0.0"
+    const val versionCode = 2
+    const val version = "1.2.0"
 
     //Only for LocalModules
     const val groupId = "com.afoxplus.android"
