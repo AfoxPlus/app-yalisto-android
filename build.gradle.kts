2149033ac51b4df97519a9f2f0300ee4d6be3ec8
Carlos Osorio <hacybeyker@gmail.com>
2023-10-31 17:54:15 -0500

    update (#17)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,15 @@
 //settings.gradle.kts
+/*allprojects {
+    configurations.all {
+        resolutionStrategy {
+            dependencySubstitution {
+                substitute(module(Deps.UI.uikit)).using(project(":uikit"))
+                substitute(module(Deps.Arch.restaurants)).using(project(":restaurants"))
+                substitute(module(Deps.Arch.products)).using(project(":products"))
+                substitute(module(Deps.Arch.orders)).using(project(":orders"))
+                substitute(module(Deps.Arch.home)).using(project(":home"))
+                //substitute(module(Deps.Arch.network)).using(project(":network"))
+            }
+        }
+    }
+}*/
