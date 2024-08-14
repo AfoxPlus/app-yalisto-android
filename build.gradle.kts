45c12fbcbbf738ab8e0fa458674d75d8cf927f2f
Valentin Mendoza <jmendozat13@gmail.com>
2024-08-14 12:37:33 -0500

    Fix firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,4 @@
-//settings.gradle.kts
-/*allprojects {
-    configurations.all {
-        resolutionStrategy {
-            dependencySubstitution {
-                substitute(module(Deps.UI.uikit)).using(project(":uikit"))
-                substitute(module(Deps.Arch.restaurants)).using(project(":restaurants"))
-                substitute(module(Deps.Arch.products)).using(project(":products"))
-                substitute(module(Deps.Arch.orders)).using(project(":orders"))
-                substitute(module(Deps.Arch.home)).using(project(":home"))
-                //substitute(module(Deps.Arch.network)).using(project(":network"))
-            }
-        }
-    }
-}*/
+plugins {
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
+}
