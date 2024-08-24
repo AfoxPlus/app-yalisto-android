a66b335dfbcb071d878e460b49d0c51d22738ab2
Valentin Mendoza Torres <jmendozat13@gmail.com>
2024-08-24 18:14:31 -0500

    Feature/notification (#33)
    * Updated versions
    
    * Refactor preferences
    
    * Added data store for show notification
    
    * Added channel
    
    * Update dependencies

--- buildSrc/src/main/kotlin/ConfigureApp.kt
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -3,8 +3,8 @@ object ConfigureApp {
     const val organization = "afoxplus"
     const val projectName = "app-yalisto-android"
     const val applicationId = "com.afoxplus.yalisto"
-    const val versionCode = 8
-    const val versionName = "6.0.3"
+    const val versionCode = 9
+    const val versionName = "6.1.0"
 
     //Only for LocalModules
     const val groupId = "com.afoxplus.android"
