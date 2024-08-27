552969331707e18ec4073e8e64ca44993927f749
Valentin Mendoza <jmendozat13@gmail.com>
2024-08-27 12:18:52 -0500

    Update notification

--- app/src/main/java/com/afoxplus/yalisto/delivery/app/YaListoFCMService.kt
+++ app/src/main/java/com/afoxplus/yalisto/delivery/app/YaListoFCMService.kt
@@ -2,7 +2,7 @@ package com.afoxplus.yalisto.delivery.app
 
 import android.app.NotificationManager
 import androidx.core.app.NotificationCompat
-import com.afoxplus.uikit.designsystem.foundations.UIKitIconTheme
+import com.afoxplus.yalisto.R
 import com.afoxplus.yalisto.cross.constants.GlobalConstants
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -14,17 +14,12 @@ class YaListoFCMService : FirebaseMessagingService() {
         showNotification(message)
     }
 
-    override fun onNewToken(token: String) {
-        super.onNewToken(token)
-        //update server
-    }
-
     private fun showNotification(message: RemoteMessage) {
         val notificationManager = getSystemService(NotificationManager::class.java)
         val notification = NotificationCompat.Builder(this, GlobalConstants.NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.icon_notification_scan)
             .setContentText(message.notification?.title)
             .setContentText(message.notification?.body)
-            .setAutoCancel(true)
             .build()
         notificationManager.notify(1, notification)
     }
