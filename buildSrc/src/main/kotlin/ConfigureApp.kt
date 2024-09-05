06fc33e5bc89ea0479fdba2f1f35e3f942909430
Valentin Mendoza <jmendozat13@gmail.com>
2024-09-04 19:27:09 -0500

    Update version

--- buildSrc/src/main/kotlin/ConfigureApp.kt
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -3,8 +3,8 @@ object ConfigureApp {
     const val organization = "afoxplus"
     const val projectName = "app-yalisto-android"
     const val applicationId = "com.afoxplus.yalisto"
-    const val versionCode = 9
-    const val versionName = "6.1.0"
+    const val versionCode = 10
+    const val versionName = "6.1.1"
 
     //Only for LocalModules
     const val groupId = "com.afoxplus.android"
